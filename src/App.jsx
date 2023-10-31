48360c0237c029647da47c9443df968e42375544
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-10-31 11:43:41 +0700

    Add: StartScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import StartScreen from "./components/StartScreen";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Main></Main>
+      <Main>
+        <StartScreen />
+      </Main>
     </div>
   );
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+function StartScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>? questions to test your react mastery</h3>
+      <button className="btn btn-ui">Let&apos;s Start!</button>
+    </div>
+  );
+}
+
+export default StartScreen;
