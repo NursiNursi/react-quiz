54ad3b37f9fc6bd1a042293b5867ad4536568127
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-10-31 12:31:19 +0700

    Install: json-server & setup questions data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import StartScreen from "./components/StartScreen";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data))
+      .catch((err) => alert(err));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <StartScreen />
+        <StartScreen numQuestions={numQuestions} />
       </Main>
     </div>
   );
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,8 +1,8 @@
-function StartScreen() {
+function StartScreen({ numQuestions }) {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
-      <h3>? questions to test your react mastery</h3>
+      <h3>{numQuestions} questions to test your react mastery</h3>
       <button className="btn btn-ui">Let&apos;s Start!</button>
     </div>
   );
