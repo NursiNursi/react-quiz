e6fd97cac523a120126a517cd5e9f258afb4f09d
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-10-31 17:15:56 +0700

    Modify: Implementing useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,64 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import StartScreen from "./components/StartScreen";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
 import Question from "./components/Question";
+import Footer from "./components/Footer";
+import NextButton from "./components/NextButton";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer": {
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    }
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
-  const [questions, setQuestions] = useState([]);
-  const [status, setStatus] = useState("loading");
-  const [index] = useState(0);
-  const numQuestions = questions.length;
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-  function handleStart() {
-    setStatus("active");
-  }
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => {
-        setQuestions(data);
-        setStatus("ready");
-      })
-      .catch(() => setStatus("error"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
   }, []);
 
   console.log(questions);
@@ -35,9 +70,20 @@ function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && (
-          <StartScreen numQuestions={numQuestions} handleStart={handleStart} />
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <>
+            <Question question={questions[index]} />
+            <Footer>
+              <NextButton
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
+          </>
+        )}
       </Main>
     </div>
   );
